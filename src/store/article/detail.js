84fd89c888595d40a4be838520802d959d1b6d4a
ningliu <ningliu@sf-express.com>
2018-12-06 17:52:21 +0800

    优化代码

--- src/store/article/detail.js
+++ src/store/article/detail.js
@@ -16,7 +16,7 @@ const setState = data => dispatch => { dispatch({ type: SET_STATE, ...data }); }
 
 // actions
 const actions = {
-  getArticle: (id) => async (dispatch) => {
+  getArticle: id => async dispatch => {
     await fetch(`/api/getdetail?id=${id}`).then(response => response.json()).then(result => {
       const data = result[0];
       dispatch(setState({ detail: data }));
